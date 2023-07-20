f86503450e67791893cf21e8835b7815b7e388f4
Rodrigo Freitas <kdigo.chr@gmail.com>
2023-07-20 09:34:34 -0300

    Update route.js

--- app/api/create-employees-table/route.js
+++ app/api/create-employees-table/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
   try {
     // const result =
-    //   await sql`CREATE TABLE employees ( Status varchar(255), Nome varchar(255), Email varchar(255), Email_Gestor varchar(255), Admissao date, Rescisao date null, Cargo varchar(255));`;
+    //   await sql`CREATE TABLE employees (Id serial primary key, Status varchar(255), Nome varchar(255), Email varchar(255), Email_Gestor varchar(255), Admissao date, Rescisao date null, Cargo varchar(255));`;
     // In case we want to drop the table, we could use the following:
     // const result =
     //   await sql`DROP TABLE employees;`;
